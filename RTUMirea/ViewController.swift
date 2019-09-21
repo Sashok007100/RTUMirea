2454e85c28f23ba8fee1608c6209eb846a586a1c
Sashok007100 <sasha.artemov.68@mail.ru>
2019-09-21 19:46:20 +0300

    Initial Commit

---
+++ RTUMirea/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RTUMirea
+//
+//  Created by Alexandr Artemov on 21/09/2019.
+//  Copyright © 2019 Alexandr Artemov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
